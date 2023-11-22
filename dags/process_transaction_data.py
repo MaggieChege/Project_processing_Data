bfb913730bb5daa5a3aa5a40313f89795081926a
MaggieChege <Shikooh.maggie@gmail.com>
2023-11-22 12:08:12 +0300

    minor changes

--- dags/process_transaction_data.py
+++ dags/process_transaction_data.py
@@ -18,20 +18,14 @@ class User:
     nTransactions: int = 0
 
     def validate(self):
-        # Validate phoneNumber (assuming it should follow a specific pattern)
         if self.phoneNumber and not self.validate_phone_number(self.phoneNumber):
             raise ValueError("Invalid phone number format")
 
-        # Validate that nTransactions is a non-negative integer
         if self.nTransactions < 0:
             raise ValueError("nTransactions cannot be negative")
 
-        # Add more validation rules as needed
-
     @staticmethod
     def validate_phone_number(phone_number: str) -> bool:
-        # Example: Simple validation to check if the phone number contains only digits
-        # You can implement more complex validation based on your requirements
         return phone_number.isdigit()
 
 @dataclass
@@ -80,6 +74,8 @@ class CSVToDB:
 
             # Check and create transaction table
             cursor.execute("""
+                -- DROP TABLE IF EXISTS public."transaction";
+
                 CREATE TABLE IF NOT EXISTS public."transaction" (
                     uuid uuid NOT NULL DEFAULT uuid_generate_v4(),
                     mobile character varying,
@@ -131,25 +127,29 @@ class CSVToDB:
     def process_csv(self, data):
         try:
             # with open(file_path, newline='') as csvfile:
-                reader = csv.DictReader(data)
-                self.ensure_tables_exist()
+            reader = csv.DictReader(data)
+            self.ensure_tables_exist()
+            
+            batch = []
+            for row in reader:
+                #Create an instance of the data class for each row
+                transaction = self.data_class(**row)
                 
-                batch = []
-                for row in reader:
-                    transaction = self.data_class(**row)
-                    user_uuid = self.user_cache.get(transaction.agentPhoneNumber)
-                    if user_uuid is None:
-                        self.insert_user(transaction)
-                        user_uuid = self.get_user_uuid(transaction.agentPhoneNumber)
-                        self.user_cache[transaction.agentPhoneNumber] = user_uuid
-                    transaction.userUuid = user_uuid
-                    batch.append(asdict(transaction))
-                    
-                    if len(batch) >= 1000:
-                        self.insert_batch(batch)
-                        batch = []
-                if batch:
+                user_uuid = self.user_cache.get(transaction.agentPhoneNumber)
+                if user_uuid is None:
+                    self.insert_user(transaction)
+                    user_uuid = self.get_user_uuid(transaction.agentPhoneNumber)
+                    self.user_cache[transaction.agentPhoneNumber] = user_uuid
+                transaction.userUuid = user_uuid
+                batch.append(asdict(transaction))
+                
+                if len(batch) >= 1000:
                     self.insert_batch(batch)
+                    batch = []
+            if batch:
+                self.insert_batch(batch)
+                
+            self.get_transaction_count() 
 
         except Exception as e:
             logging.error("Failed to process Data")
@@ -171,6 +171,15 @@ class CSVToDB:
         self.cursor.execute('''SELECT uuid FROM "user" WHERE "phoneNumber" = %s''', (phone_number,))
         result = self.cursor.fetchone()
         return result[0] if result else None
+    
+    
+    def get_transaction_count(self):
+        self.cursor.execute('''SELECT count(*) FROM "transaction" ''')
+        count = self.cursor.fetchone()[0]
+        print(f'The Transaction count is: {count}')
+        self.cursor.close()
+        
+
 
 
     def insert_batch(self, batch):
